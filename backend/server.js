b3ce65ffb590619c7e4a0df09d9207b335714ef4
Quino <joaquinballara02@gmail.com>
2024-12-23 01:40:33 -0300

    routes in server

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const authRoutes = require('./routes/authRoutes')
+const groupRoutes = require('./routes/groupRoutes')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/auth', authRoutes)
+app.use('/api/group', groupRoutes)
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
